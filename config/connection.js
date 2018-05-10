0ba69bf21480f50e331bbdbc000689e6e3a1cf72
BBITguru <jparker@bbit.guru>
2018-05-10 18:19:57 -0400

    100% workeding

--- config/connection.js
+++ config/connection.js
@@ -18,17 +18,29 @@ var mysql = require("mysql");
 // });
 
 // JAWSDB:mysql
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-connection.connect();
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+    port: 8889
+  })
+}
 
-connection.end();
+// connection.connect();
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err;
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection.end();
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
